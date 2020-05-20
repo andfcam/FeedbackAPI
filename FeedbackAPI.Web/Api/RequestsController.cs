c397103b8a8d226420918e003bf7e846e757d74f
Andrew Cameron <Andrew.Cameron@landmark.co.uk>
2020-05-20 16:00:18 +0100

    Configured Autofac and a web API controller.

---
+++ FeedbackAPI.Web/Api/RequestsController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FeedbackAPI.Web.Api
+{
+    public class RequestsController : ApiController
+    {
+    }
+}
