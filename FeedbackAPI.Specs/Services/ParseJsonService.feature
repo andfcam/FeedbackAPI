e662af4f45fe68e8d321107e27dabed1543258f6
Andrew Cameron <Andrew.Cameron@landmark.co.uk>
2020-05-28 13:16:06 +0100

    Implemented five of the test steps. Adjusted ParseJsonService to be more easily tested.

--- FeedbackAPI.Specs/Services/ParseJsonService.feature
+++ FeedbackAPI.Specs/Services/ParseJsonService.feature
@@ -6,36 +6,27 @@
 
 Scenario: Create and Site requests are correctly identified by their request 'header'
 	Given I receive data to create a site
-	When I simulate the new request
+	When I parse the new request
 	Then the request should be identified as a create site request
 
 Scenario: Update and Facility requests are correctly identified by their request 'header'
 	Given I receive data to update a facility
-	When I simulate the new request
+	When I parse the new request
 	Then the request should be identified as an update facility request
 
 Scenario: Assigning a site id to a new create site request
-	Given I receive data to create a site
-	And the data has no site id
-	When I simulate the new request
+	Given I receive data to create a site with no site id
+	When I parse the new request
 	Then the request should have a site id
 
 Scenario: Rejecting a new update site request with no valid site id
-	Given I receive data to update a site
-	And the data has no site id
-	When I simulate the new request
+	Given I receive data to update a site with no site id
+	When I parse the new request
 	Then the request should be rejected
 
-Scenario: Assigning facility ids to a new create facility request
-	Given I receive data to create facilities
-	And the data has no facility ids
-	When I simulate the new request
-	Then the request should have facility ids
-
 Scenario: Rejecting a new update facility request with no valid facility ids
-	Given I receive data to update facilities
-	And the data has no facility ids
-	When I simulate the new request
+	Given I receive data to update facilities with no facility ids
+	When I parse the new request
 	Then the request should be rejected
 
 #Scenario: Partially valid facility data
---
+++ FeedbackAPI.Specs/Services/ParseJsonServiceSteps.cs
@@ -0,0 +1,81 @@
+﻿using Xunit;
+using FeedbackAPI.Data.Models;
+using FeedbackAPI.Web.Models;
+using FeedbackAPI.Web.Services;
+using TechTalk.SpecFlow;
+
+namespace FeedbackAPI.Specs.Services
+{
+    [Binding]
+    public class ParseJsonServiceSteps
+    {
+        private readonly ScenarioContext _scenarioContext;
+        private string _data;
+        private dynamic _model;
+        private StatusType _status;
+        private Request _request;
+
+        public ParseJsonServiceSteps(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given(@"I receive data to create a site with no site id")]
+        public void GivenIReceiveDataToCreateASiteWithNoSiteId()
+        {
+            _data = ParseJsonService.ReadFromFile($"~/App_Data/request-1.json");
+        }
+
+        [Given(@"I receive data to update a site with no site id")]
+        public void GivenIReceiveDataToUpdateASiteWithNoSiteId()
+        {
+            _data = ParseJsonService.ReadFromFile($"~/App_Data/request-5.json");
+        }
+
+        [Given(@"I receive data to create facilities with no facility ids")]
+        public void GivenIReceiveDataToCreateFacilitiesWithNoFacilityIds()
+        {
+            _data = ParseJsonService.ReadFromFile($"~/App_Data/request-2.json");
+        }
+
+        [Given(@"I receive data to update facilities with no facility ids")]
+        public void GivenIReceiveDataToUpdateFacilitiesWithNoFacilityIds()
+        {
+            _data = ParseJsonService.ReadFromFile($"~/App_Data/request-6.json");
+        }
+
+        [When(@"I parse the new request")]
+        public void WhenIParseTheNewRequest()
+        {
+            _model = ParseJsonService.Deserialise(_data);
+            _status = ParseJsonService.Validate(_model);
+            _request = ParseJsonService.Parse(_model, _data, _status);
+        }
+        
+        [Then(@"the request should be identified as a create site request")]
+        public void ThenTheRequestShouldBeIdentifiedAsACreateSiteRequest()
+        {
+            Assert.IsType<SiteRequest>(_model);
+            Assert.Equal(ActionType.Create, _request.Action);
+        }
+        
+        [Then(@"the request should be identified as an update facility request")]
+        public void ThenTheRequestShouldBeIdentifiedAsAnUpdateFacilityRequest()
+        {
+            Assert.IsType<FacilityRequest>(_model);
+            Assert.Equal(ActionType.Update, _request.Action);
+        }
+        
+        [Then(@"the request should have a site id")]
+        public void ThenTheRequestShouldHaveASiteId()
+        {
+            Assert.NotEqual(0, _model.Site.Id);
+        }
+        
+        [Then(@"the request should be rejected")]
+        public void ThenTheRequestShouldBeRejected()
+        {
+            Assert.Equal(StatusType.Rejected, _request.Status);
+        }
+    }
+}
